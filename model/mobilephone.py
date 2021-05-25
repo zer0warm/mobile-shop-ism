828c888a2beddbfbeecab145873d1246fc231056
zer0warm <dqhieu.2016@gmail.com>
2021-05-25 23:23:25 +0700

    Model: Update mobilephone class
    This commit includes the ability to insert the data to database.

--- model/mobilephone.py
+++ model/mobilephone.py
@@ -1,22 +1,27 @@
+from model.database import Database
+
 class Mobilephone:
     __id = 0
 
-    def __init__(self, brand, name, storage, color, price):
+    def __init__(self, brand, name, storage, color, price, qty):
         self.__id = Mobilephone.__id
         self.__brand = brand
         self.__name = name
         self.__storage = storage
         self.__color = color
         self.__price = price
+        self.__quantity = qty
         Mobilephone.__id += 1
 
     def speak(self):
         return self.__dict__
 
-    def export_info(self, db_obj):
-        query = ''
-        variables = ()
-        db_obj.execute(query, variables)
+    def export_info(self):
+        query = '''insert into phones (brand, name, storage, color, price, qty)
+                   values (?, ?, ?, ?, ?, ?);'''
+        variables = (self.__brand, self.__name, self.__storage,
+                     self.__color, self.__price, self.__quantity)
+        Database.execute(query, variables)
 
     @staticmethod
     def import_info(db_obj):
