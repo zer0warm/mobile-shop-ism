abe5e092cb1a41037e4c2504651c5b28497cf877
zer0warm <dqhieu.2016@gmail.com>
2021-05-26 01:33:43 +0700

    Update main
    Add supports for the new changes in the application. Packing algorithm
    is used instead, and the sections (sidebar, logbar, functional) are
    packed to fixed size geometries. It is also where to initialize the
    connection with the database.

--- main.py
+++ main.py
@@ -1,21 +1,40 @@
 import tkinter as tk
 from tkinter import ttk
 
-import windows.sidebar as sb
-import windows.winlog as log
+from windows.sidebar import Sidebar
+from windows.winlog import WinLog
+from windows.base import WinBase
+from windows.manager import WinManager
+
+from model.mobilephone import Mobilephone
+from model.database import Database
 
 if __name__ == '__main__':
     root = tk.Tk()
 
     root.title("which group?'s phone-shop-ism")
-    root.geometry(f'{root.winfo_screenwidth()}x{root.winfo_screenheight()}+0+0')
+    root.geometry(f'900x{root.winfo_screenheight()}+100+0')
 
-    logbar = log.WinLog(root)
-    logbar.grid(row=10, column=1, columnspan=5)
+    sidebar_frame = tk.LabelFrame(root, width=300, height=900, bd=10)
+    functional_frame = tk.LabelFrame(root, width=880, height=600, bd=10, background='green')
+    log_frame = tk.LabelFrame(root, width=880, height=300, bd=10)
 
-    sidebar = sb.Sidebar(root)
+    logbar = WinLog(log_frame)
+    logbar.pack(expand=True, fill='both')
+
+    sidebar = Sidebar(sidebar_frame)
     sidebar.create_widgets()
     sidebar.display_widgets()
-    sidebar.grid(row=0, column=0, rowspan=10, sticky='nsew')
-    
+    sidebar.pack(expand=True, fill='both')
+
+    WinManager.set_master(functional_frame)
+    WinManager.set_root(root)
+
+    sidebar_frame.pack(expand=True, fill='both', side='left')
+    functional_frame.pack(expand=True, fill='both', side='top')
+    functional_frame.pack_propagate(0)
+    log_frame.pack(expand=True, fill='both', side='bottom')
+
+    Database.init('sample.db')
+
     root.mainloop()
