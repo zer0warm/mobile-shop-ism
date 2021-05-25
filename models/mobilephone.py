109d0adf1791e993bc3c40456a7ada495318418e
zer0warm <dqhieu.2016@gmail.com>
2021-05-26 05:28:42 +0700

    Mobilephone: Add update query

--- models/mobilephone.py
+++ models/mobilephone.py
@@ -1,20 +1,18 @@
 from models.database import Database
+from windows.log import WinLog
 
 class Mobilephone:
-    __id = 0
-
-    def __init__(self, brand, name, storage, color, price, qty):
-        self.__id = Mobilephone.__id
+    def __init__(self, brand, name, storage, color, price, qty, id_=None):
+        self.__id = id_
         self.__brand = brand
         self.__name = name
         self.__storage = storage
         self.__color = color
         self.__price = price
         self.__quantity = qty
-        Mobilephone.__id += 1
 
     def speak(self):
-        return self.__dict__
+        WinLog.update_log(f'Model: {self.__dict__}')
 
     def export_info(self):
         query = '''insert into phones (brand, name, storage, color, price, qty)
@@ -22,6 +20,16 @@ class Mobilephone:
         variables = (self.__brand, self.__name, self.__storage,
                      self.__color, self.__price, self.__quantity)
         Database.execute(query, variables)
+        WinLog.update_log('Database: Export query executed.')
+
+    def update_info(self):
+        query = '''update phones
+                   set (brand, name, storage, color, price, qty) = (?, ?, ?, ?, ?, ?)
+                   where id = ?;'''
+        variables = (self.__brand, self.__name, self.__storage,
+                     self.__color, self.__price, self.__quantity, self.__id)
+        Database.execute(query, variables);
+        WinLog.update_log('Database: Update query executed.')
 
     @staticmethod
     def import_info(db_obj):
