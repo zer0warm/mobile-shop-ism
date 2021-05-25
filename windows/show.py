3aafcc8f0c4fb5a073124b579ea5268352dce640
zer0warm <dqhieu.2016@gmail.com>
2021-05-26 01:32:26 +0700

    Windows: Add WinShow
    This class implements the listing functionality: it shows a list of
    available mobilephones in the store, by querying the database.

---
+++ windows/show.py
@@ -0,0 +1,36 @@
+import tkinter as tk
+import tkinter.ttk as ttk
+
+from model.mobilephone import Mobilephone
+from model.database import Database
+
+class WinShow(tk.LabelFrame):
+    def __init__(self, master):
+        super().__init__(master)
+        self['text'] = 'List of mobile phones'
+
+        self.create_widgets()
+        self.display_widgets()
+
+    def create_widgets(self):
+        headings = ('id', 'brand', 'name', 'color', 'storage', 'price', 'qty')
+        self.__tree = ttk.Treeview(self, columns=headings, show='headings')
+
+        for h in headings:
+            if h != 'name':
+                self.__tree.column(h, width=75)
+            self.__tree.column(h, anchor='center')
+            self.__tree.heading(h, text=h.upper())
+
+        Database.execute('SELECT * FROM Phones')
+        for row in Database.get_result(all_=True):
+            self.__tree.insert('', 'end', values=row)
+
+    def display_widgets(self):
+        self.__tree.pack(expand=True, fill='both', side='top')
+
+    def get_user_input(self):
+        data = {}
+        for label, textentry in self.__widgets:
+            data[label['text']] = textentry
+        return data
