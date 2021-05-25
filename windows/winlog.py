39d77d77802a9ba01a3166dd098489b2cd33cfb4
zer0warm <dqhieu.2016@gmail.com>
2021-05-26 01:23:54 +0700

    Windows: Update WinLog class
    Make the update function static, and the log object a class member, so
    that external modules might log information. The log bar will be
    initialized once at main for tkinter's packing purpose.

--- windows/winlog.py
+++ windows/winlog.py
@@ -3,27 +3,34 @@ import tkinter as tk
 from tkinter import ttk
 
 class WinLog(tk.LabelFrame):
+    __log = None
+
     def __init__(self, master):
         super().__init__(master)
-        self['text'] = 'Log'
 
         self.__create_widgets()
         self.__display_widgets()
 
-        for i in range(10):
-            self.update_log(i)
-
     def __create_widgets(self):
-        self.__log = tk.Text(self, height=3)
-        scr = ttk.Scrollbar(self, orient='vertical', command=self.__log.yview)
-        self.__log['yscrollcommand'] = scr.set
+        WinLog.__log = tk.Text(self, height=5)
+        scr = ttk.Scrollbar(self, orient='vertical', command=WinLog.__log.yview)
+        WinLog.__log['yscrollcommand'] = scr.set
     
     def __display_widgets(self):
-        self.__log.pack(expand=True, fill=tk.BOTH)
+        WinLog.__log.pack(expand=True, fill=tk.BOTH)
 
-    def update_log(self, message):
-        self.__log['state'] = tk.NORMAL
+    @staticmethod
+    def update_log(message):
+        WinLog.__log['state'] = tk.NORMAL
         timestamp = time.strftime('[%F %T]')
-        self.__log.insert('end', f'{timestamp} {message}\n')
-        self.__log.see('end')
-        self.__log['state'] = tk.DISABLED
+        WinLog.__log.insert('end', f'{timestamp} {message}\n')
+        WinLog.__log.see('end')
+        WinLog.__log['state'] = tk.DISABLED
+
+    #@staticmethod
+    #def update_log_tkinter(*args):
+    #    WinLog.__log['state'] = tk.NORMAL
+    #    timestamp = time.strftime('[%F %T]')
+    #    WinLog.__log.insert('end', f'{timestamp} Error {dir(args[1])}\n')
+    #    WinLog.__log.see('end')
+    #    WinLog.__log['state'] = tk.DISABLED
