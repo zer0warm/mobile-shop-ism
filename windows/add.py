c9008bc8e032c8d08a82f40133bafab687646079
zer0warm <dqhieu.2016@gmail.com>
2021-05-26 22:05:47 +0700

    WinAdd: Move handlers into the class

--- windows/add.py
+++ windows/add.py
@@ -2,33 +2,14 @@ import tkinter as tk
 
 from models.mobilephone import Mobilephone
 
-def add_button_handler(data):
-    def callback():
-        brand = data['Brand'].get()
-        name = data['Name'].get()
-        storage = data['Storage'].get()
-        color = data['Color'].get()
-        price = data['Price'].get()
-        quantity = data['Quantity'].get()
-        new_phone = Mobilephone(brand, name, storage, color, float(price), quantity)
-        new_phone.speak()
-        new_phone.export_info()
-    return callback
-
-def clear_button_handler(data):
-    def callback():
-        for textentry in data.values():
-            textentry.delete(0, 'end')
-    return callback
-
 class WinAdd(tk.LabelFrame):
     def __init__(self, master):
         super().__init__(master)
         self['text'] = 'Add new mobile phone'
-        self.create_widgets()
-        self.display_widgets()
+        self.__create_widgets()
+        self.__display_widgets()
 
-    def create_widgets(self):
+    def __create_widgets(self):
         self.__widgets = [
             (tk.Label(self, text='Brand'), tk.Entry(self)),
             (tk.Label(self, text='Name'), tk.Entry(self)),
@@ -40,13 +21,13 @@ class WinAdd(tk.LabelFrame):
         self.__add_button = tk.Button(
                 self,
                 text='Add',
-                command=add_button_handler(self.get_user_input()))
+                command=self.__add_button_handler)
         self.__clear_button = tk.Button(
                 self,
                 text='Clear',
-                command=clear_button_handler(self.get_user_input()))
+                command=self.__clear_button_handler)
 
-    def display_widgets(self):
+    def __display_widgets(self):
         for i in range(len(self.__widgets)):
             label, textentry = self.__widgets[i]
             label.grid(row=i, column=0)
@@ -54,8 +35,27 @@ class WinAdd(tk.LabelFrame):
         self.__add_button.grid(row=i+1, column=4, sticky='nsew')
         self.__clear_button.grid(row=i+1, column=5, sticky='nsew')
 
-    def get_user_input(self):
+    def __get_user_input(self):
         data = {}
         for label, textentry in self.__widgets:
             data[label['text']] = textentry
         return data
+
+    def __add_button_handler(self):
+        data = self.__get_user_input()
+
+        brand = data['Brand'].get()
+        name = data['Name'].get()
+        storage = data['Storage'].get()
+        color = data['Color'].get()
+        price = data['Price'].get()
+        quantity = data['Quantity'].get()
+
+        new_phone = Mobilephone(brand, name, storage, color, float(price), quantity)
+        new_phone.speak()
+        new_phone.export_info()
+
+    def __clear_button_handler(self):
+        data = self.__get_user_input()
+        for textentry in data.values():
+            textentry.delete(0, 'end')
